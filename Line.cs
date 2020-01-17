05c7e3d93653e9ca6d0d951a63440f07bdfaf6e7
abercr0mby <abercromby@gmail.com>
2020-01-17 08:57:47 +0000

    Minor changes

--- Line.cs
+++ Line.cs
@@ -2,5 +2,14 @@ using System;
 
 class Line
 {
-    
+    private XAxisIntercept int[2] { get; set; };
+    private YAxisIntercept int[2] { get; set; };
+    private List<Asteroid> Asteroids { get; set; }
+
+    public Line(int[2] xAxisIntercept, int[2] yAxisIntercept)
+    {
+      XAxisIntercept = xAxisIntercept;
+      YAxisIntercept = yAxisIntercept;
+      Asteroids = new List<Asteroids>();
+    }
 }
