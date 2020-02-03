da324807abcf6a0b7d60974e6a9248e707b537b9
abercr0mby <abercromby@gmail.com>
2020-02-03 14:18:19 +0000

    Completed Day 12

--- DayTwelve/DayTwelve.cs
+++ DayTwelve/DayTwelve.cs
@@ -4,7 +4,25 @@ using System.Linq;
 class DayTwelve
 {
 
-  public int PartTwo()
+  public long PartTwo()
+  {
+
+    RunTestsPartTwo();
+
+    var systemData = @"<x=-3, y=15, z=-11>
+<x=3, y=13, z=-19>
+<x=-13, y=18, z=-2>
+<x=6, y=0, z=-1>";
+
+  var partTwo = new DayTwelvePartTwo(systemData);
+  var result = partTwo.FindRepeat();  
+
+  Console.WriteLine("steps doned: " + result);
+
+    return result;
+  }
+
+  public void RunTestsPartTwo()
   {
     var systemData = @"<x=-1, y=0, z=2>
 <x=2, y=-10, z=-7>
@@ -12,13 +30,31 @@ class DayTwelve
 <x=3, y=5, z=-1>";
 
   var partTwo = new DayTwelvePartTwo(systemData);
+  var result = partTwo.FindRepeat();  
+  if(result == 2772)
+  {
+    Console.WriteLine("Passed day twelve part two test one");
+  }
+  else
+  {
+    throw new System.Exception("Failed. Should ahve been 2272 was " + result);
+  }
 
-//for(var i = 0; i< 100; i++)
-  partTwo.PerformSteps(4686774900);
+    systemData = @"<x=-8, y=-10, z=0>
+<x=5, y=5, z=10>
+<x=2, y=-7, z=3>
+<x=9, y=-8, z=-3>";
 
-  Console.WriteLine("steps doned!");
-
-    return 0;
+  partTwo = new DayTwelvePartTwo(systemData);
+  result = partTwo.FindRepeat();  
+  if(result == 4686774924)
+  {
+    Console.WriteLine("Passed day twelve part two test two");
+  }
+  else
+  {
+    throw new System.Exception("Failed. Should have been 4686774924 was " + result);
+  }
   }
 
   public int RunTestsAndGetResultPartOne() 
@@ -68,8 +104,6 @@ class DayTwelve
     {
       Console.WriteLine("Total Energy: " + result);
     }
-
-
     
   }
 }
--- DayTwelve/DayTwelvePartTwo.cs
+++ DayTwelve/DayTwelvePartTwo.cs
@@ -6,18 +6,22 @@ class DayTwelvePartTwo
   public List<OrbitalBody> OrbitalBodies { get; set; }
 
   public int[] XPositions { get; set; }
-
   public int[] YPositions { get; set; }
-
-  public int[] ZPositions { get; set; }
+  public int[] ZPositions { get; set; }  
 
   public int[] XVelocities { get; set; }
-
   public int[] YVelocities { get; set; }
-
   public int[] ZVelocities { get; set; }
 
+  public Dictionary<string, long> XLog { get; set; }
+  public Dictionary<string, long> YLog { get; set; }
+  public Dictionary<string, long> ZLog { get; set; }
+
   public List<int[]> Pairings { get; set; }
+
+  public long XRepeatStep { get; set; }
+  public long YRepeatStep { get; set; }
+  public long ZRepeatStep { get; set; }
 
   public DayTwelvePartTwo(string systemData)
   {
@@ -26,6 +30,25 @@ class DayTwelvePartTwo
 
       CreateBodiesFromData(systemData);
 
+      XLog = new Dictionary<string, long>();
+      YLog = new Dictionary<string, long>();
+      ZLog = new Dictionary<string, long>();
+
+      var xKey = "";
+      var yKey = "";
+      var zKey = "";
+
+      for(var i = 0; i < OrbitalBodies.Count; i++)
+      {
+        xKey = xKey + OrbitalBodies[i].XPosition.ToString() + ":" + OrbitalBodies[i].XVelocity.ToString() + ":";
+        yKey = yKey + OrbitalBodies[i].YPosition.ToString() + ":" + OrbitalBodies[i].YVelocity.ToString() + ":";
+        zKey = zKey + OrbitalBodies[i].ZPosition.ToString() + ":" + OrbitalBodies[i].ZVelocity.ToString() + ":";
+      }
+
+      XLog.Add(xKey, 0);
+      YLog.Add(yKey, 0);
+      ZLog.Add(zKey, 0);              
+
       XPositions = new int[OrbitalBodies.Count];
       YPositions = new int[OrbitalBodies.Count];
       ZPositions = new int[OrbitalBodies.Count];
@@ -33,9 +56,9 @@ class DayTwelvePartTwo
       YVelocities = new int[OrbitalBodies.Count];
       ZVelocities = new int[OrbitalBodies.Count];
 
-      for(var i = 0; i < XVelocities.Length; i++)
+      for(var i = 0; i < XPositions.Length; i++)
       {
-          for(var j = i+1; j < XVelocities.Length; j++)
+          for(var j = i+1; j < XPositions.Length; j++)
           {
               Pairings.Add(new int[] {i, j});
           }
@@ -55,50 +78,148 @@ class DayTwelvePartTwo
       }
   }
 
+  public long FindRepeat()
+  {
+    var count = 0;
+
+    do
+    {
+      count++;
+
+      ApplyGravity();
+      MoveCheckAndLog(count);
+      if(XRepeatStep > 0 && YRepeatStep > 0 && ZRepeatStep > 0)
+      {
+        return CalculateOveralRepeatStep();
+      }
+
+    }while( true );
+  }
+
+
   public void PerformSteps(long steps)
   {
     for(long i = 0; i < steps; i++)
     {
-      PerformStep();
+      ApplyGravity();
+      MoveCheckAndLog(i);
+      if(XRepeatStep > 0 && YRepeatStep > 0 && ZRepeatStep > 0)
+      {
+        CalculateOveralRepeatStep();
+      }
     }
   }
 
-  public void PerformStep()
+  public long CalculateOveralRepeatStep()
+  {
+    return DetermineLcm(DetermineLcm(XRepeatStep, YRepeatStep), ZRepeatStep);
+  }
+
+  public void MoveCheckAndLog(long step)
+  {
+    var xRepeat = true;
+    var yRepeat = true;
+    var zRepeat = true;
+
+    var xKey = "";
+    var yKey = "";
+    var zKey = "";
+
+    for(int i = 0; i < XPositions.Length; i++)
+    {
+      if(XRepeatStep == 0)
+      {
+        XPositions[i] += XVelocities[i];
+        xKey += XPositions[i].ToString() + ":" + XVelocities[i].ToString() + ":";
+      }
+
+      if(YRepeatStep == 0)
+      {
+        YPositions[i] += YVelocities[i];
+        yKey += YPositions[i].ToString() + ":" + YVelocities[i].ToString() + ":";
+      }
+      
+      if(ZRepeatStep == 0)
+      {
+        ZPositions[i] += ZVelocities[i];
+        zKey += ZPositions[i].ToString() + ":" + ZVelocities[i].ToString() + ":";
+      }
+    }
+
+    if(XRepeatStep == 0)
+    {
+      if(!XLog.ContainsKey(xKey))
+      {
+        XLog.Add(xKey, step);
+      }
+      else
+      {
+        XRepeatStep = step;
+      }
+    }
+
+    if(YRepeatStep == 0)
+    {
+      if(!YLog.ContainsKey(yKey))
+      {
+        YLog.Add(yKey, step);
+      }
+      else
+      {
+        YRepeatStep = step;
+      }
+    }
+
+    if(ZRepeatStep == 0)
+    {
+      if(!ZLog.ContainsKey(zKey))
+      {
+        ZLog.Add(zKey, step);
+      }
+      else
+      {
+        ZRepeatStep = step;
+      }
+    }        
+
+  }
+
+  public void ApplyGravity()
   {
     for(var i = 0; i < Pairings.Count; i++)
     {
       if (XPositions[Pairings[i][0]] > XPositions[Pairings[i][1]])
       {
-        XPositions[Pairings[i][0]] --;
-        XPositions[Pairings[i][1]] ++;
-      }/*
+        XVelocities[Pairings[i][0]] --;
+        XVelocities[Pairings[i][1]] ++;
+      }
       else if (XPositions[Pairings[i][0]] < XPositions[Pairings[i][1]])
       {
-        XPositions[Pairings[i][0]] ++;
-        XPositions[Pairings[i][1]] --;
+        XVelocities[Pairings[i][0]] ++;
+        XVelocities[Pairings[i][1]] --;
       }
 
       if (YPositions[Pairings[i][0]] > YPositions[Pairings[i][1]])
       {
-        YPositions[Pairings[i][0]] --;
-        YPositions[Pairings[i][1]] ++;
+        YVelocities[Pairings[i][0]] --;
+        YVelocities[Pairings[i][1]] ++;
       }
       else if (YPositions[Pairings[i][0]] < YPositions[Pairings[i][1]])
       {
-        YPositions[Pairings[i][0]] ++;
-        YPositions[Pairings[i][1]] --;
+        YVelocities[Pairings[i][0]] ++;
+        YVelocities[Pairings[i][1]] --;
       }  
 
       if (ZPositions[Pairings[i][0]] > ZPositions[Pairings[i][1]])
       {
-        ZPositions[Pairings[i][0]] --;
-        ZPositions[Pairings[i][1]] ++;
+        ZVelocities[Pairings[i][0]] --;
+        ZVelocities[Pairings[i][1]] ++;
       }
       else if (ZPositions[Pairings[i][0]] < ZPositions[Pairings[i][1]])
       {
-        ZPositions[Pairings[i][0]] ++;
-        ZPositions[Pairings[i][1]] --;
-      }*/                
+        ZVelocities[Pairings[i][0]] ++;
+        ZVelocities[Pairings[i][1]] --;
+      }               
      } 
   }
 
@@ -125,6 +246,28 @@ class DayTwelvePartTwo
       OrbitalBodies.Add(body);
     }
   }
+
+    public static long DetermineLcm(long a, long b)
+    {
+        long num1, num2;
+        if (a > b)
+        {
+            num1 = a; num2 = b;
+        }
+        else
+        {
+            num1 = b; num2 = a;
+        }
+
+        for (int i = 1; i < num2; i++)
+        {
+            if ((num1 * i) % num2 == 0)
+            {
+                return i * num1;
+            }
+        }
+        return num1 * num2;
+    }  
 
 
 }
